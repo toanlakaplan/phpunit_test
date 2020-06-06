610db8930c3da1d2483f40969a8a3fa187d88ca0
Toan Le <Toan.Le-Anh@aspectworld.com>
2020-06-06 22:56:39 +0700

    Add a test

--- myapp/tests/AverageTest.php
+++ myapp/tests/AverageTest.php
@@ -23,4 +23,10 @@ class AverageTest extends TestCase
         $numbers = [3, 7, 6, 1, 5];
         $this->assertEquals(5, $this->Average->median($numbers));
     }
+
+    public function testCalculationOfMedian2()
+    {
+        $numbers = [3, 7, 6, 1, 5];
+        $this->assertEquals(5, $this->Average->median($numbers));
+    }
 }
