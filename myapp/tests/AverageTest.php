31b07ab282d049d2643ac27061caa3b2a601bb30
Toan Le <52987734+toanlakaplan@users.noreply.github.com>
2020-06-07 17:41:16 +0700

    Update AverageTest.php

--- myapp/tests/AverageTest.php
+++ myapp/tests/AverageTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 class AverageTest extends TestCase
 {
     protected $Average;
-    //asdfa asdfasf
+    //asdfa asdfasf adsfas
     public function setUp()
     {
       //a
