bb5de171a21944c3922cd1fda07a9f242fcf9a05
Toan Le <Toan.Le-Anh@aspectworld.com>
2020-06-08 10:30:44 +0700

    ass

--- myapp/tests/AverageTest.php
+++ myapp/tests/AverageTest.php
@@ -14,7 +14,7 @@ class AverageTest extends TestCase
 
     public function testCalculationOfMean()
     {
-        // hallo 2 3 3 4 54 6 234 2342 asdf asdfa asdfsad fasfs fa fasfas  asfas
+        // hallo 2 3 3 4 54 6 234 2342 asdf asdfa asdfsad fasfs fa fasfas  asfas asfs
         $numbers = [3, 7, 6, 1, 5];
         $this->assertEquals(4.4, $this->Average->mean($numbers));
     }
